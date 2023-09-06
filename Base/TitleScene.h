42cbf79ed14599ade7ed26bbcd4962cce2390d51
イイダ_ユウキ <k021g1333@g.neec.ac.jp>
2023-09-06 10:34:39 +0900

    タイトルの処理が消えたので

--- Base/TitleScene.h
+++ Base/TitleScene.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "IScene.h"
+#include <memory>
 
 class TitleScene :
     public IScene
 {
+private:
+    //タイトル画像ハンドル
+    uint32_t titleImageHandle_ = 0;
+
 public:
     // 関数
     void Initialize(void) override;
