789438459956253fde43132b86acdcbcee5f04d5
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-09-06 10:12:06 +0900

    びちょうせい

--- Apps/PlayerAttack.cpp
+++ Apps/PlayerAttack.cpp
@@ -55,11 +55,12 @@ void PlayerMowAttack::Draw(void)
 {
     // çUåÇîÕàÕÇâ¬éãâªÅB
     //DrawBox((int32_t)(position_.x - radius_.x), (int32_t)(position_.y - radius_.y), (int32_t)(position_.x + radius_.x), (int32_t)(position_.y + radius_.y), Util::Color::BLUE, true);
-    DrawCircle((int32_t)position_.x, (int32_t)position_.y, (int32_t)radius_.x, Util::Color::BLUE, true, 1);
-    //DrawFormatString((int32_t)position_.x - 5, (int32_t)position_.y - 70, Util::Color::YELLOW, state_dir_ == Direction::RIGHT ? "âE" : "ç∂");
+    //DrawCircle((int32_t)position_.x, (int32_t)position_.y, (int32_t)radius_.x, Util::Color::BLUE, true, 1);
+    DrawFormatString((int32_t)position_.x - 5, (int32_t)position_.y - 70, Util::Color::YELLOW, state_dir_ == Direction::RIGHT ? "âE" : "ç∂");
+    DrawFormatString((int32_t)position_.x - 5, (int32_t)position_.y - 90, Util::Color::YELLOW, "%f",rotation_);
 
     //// çUåÇÇÃï`âÊ
-    //state_dir_ == Direction::RIGHT ?
-    //    DrawRotaGraph((int32_t)position_.x, (int32_t)position_.y, kPngScale_, rotation_, png_mowAttack_, true) :
-    //    DrawRotaGraph((int32_t)position_.x, (int32_t)position_.y, kPngScale_, -rotation_, png_mowAttack_, true);
+    state_dir_ == Direction::RIGHT ?
+        DrawRotaGraph((int32_t)position_.x, (int32_t)position_.y, kPngScale_, rotation_, png_mowAttack_, true) :
+        DrawRotaGraph((int32_t)position_.x, (int32_t)position_.y, kPngScale_, rotation_, png_mowAttack_, true,true,true);
 }
